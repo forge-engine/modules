b2395aa4bbccad9791cf761fd0e4b5fb2424c385
Jeremias Nunez <jeremias2@gmail.com>
2025-02-25 00:08:04 -0400

    initial readme

---
+++ Logger/LoggerModule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Forge\Modules\Logger;
+
+use Forge\Core\Contracts\Modules\LoggerInterface;
+use Forge\Core\Contracts\Modules\ModulesInterface;
+use Forge\Core\Configuration\Config;
+use Forge\Core\DependencyInjection\Container;
+use Forge\Core\Helpers\Debug;
+
+class LoggerModule extends ModulesInterface
+{
+    public function register(Container $container): void
+    {
+        $config = $container->get(Config::class);
+        $logger = new FileLogger($config);
+        $container->instance(LoggerInterface::class, $logger);
+        Debug::addEvent("[LoggerModule] Registered", "start");
+    }
+}
